2b1c42aa819854e365e11ae3adf6c4e7fa9d54d3
Astgenne <danicandido02@gmail.com>
2025-03-21 19:17:15 +0100

    initial commit

---
+++ projects/ngx-game-of-life-bg/src/lib/game-of-life-bg/game-of-life-bg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'game-of-life-bg',
+    templateUrl: './game-of-life-bg.component.html',
+    styleUrls: ['./game-of-life-bg.component.css']
+})
+export class GameOfLifeBgComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameOfLifeBgComponent } from 'ngx-game-of-life-bg';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, GameOfLifeBgComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-game-of-life';
 }
